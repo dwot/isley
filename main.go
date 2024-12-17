3a295acb83b71e8d6e5a8f0c25b3d9694cfc2892
dwot <68145+dwot@users.noreply.github.com>
2024-12-16 23:41:41 -0500

    v0.1.0
    Working on Fixing up Github Release

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 	"time"
 )
 
-//go:embed migrations/*.sql web/templates/* web/static/* fonts/*
+//go:embed model/migrations/*.sql web/templates/* web/static/* fonts/*
 var embeddedFiles embed.FS
 
 func main() {
